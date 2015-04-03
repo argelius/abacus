38ee788c99844b93b8264ef3eb80ed3a73d83ab9
Andreas Argelius <andreas@asial.co.jp>
2015-04-03 22:18:27 +0900

    Start

--- gulpfile.js
+++ gulpfile.js
@@ -1,68 +1,18 @@
 var gulp = require('gulp'),
-    concat = require('gulp-concat'),
-    sourcemaps = require('gulp-sourcemaps'),
-    uglify = require('gulp-uglify'),
-    cached = require('gulp-cached'),
-    jshint = require('gulp-jshint'),
-    bower = require('gulp-bower'),
-    connect = require('gulp-connect'),
-    html2js = require('gulp-html2js'),
-    path = require('path');
-
-var templates = [
-  'src/templates/*.html'
-]
+  traceur = require('gulp-traceur'),
+  concat = require('gulp-concat'),
+  uglify = require('gulp-uglify')
 
 var sourceFiles = [
-  'src/templates/*.js',
-  'src/abacus.js', 
-  'src/services/*.js',
-  'src/directives/*.js'
+  'src/*.es6'
 ];
 
-var examples = [
-  'examples/*.html',
-  'examples/**/*.html'
-];
-
-var port = 3002;
-
-gulp.task('templates', function() {
-  gulp.src(templates)
-    .pipe(html2js({base: path.join(__dirname, '/src'), outputModuleName: 'abacus.templates', useStrict: true, quoteChar: '\''}))
-    .pipe(concat('templates.js'))
-    .pipe(gulp.dest('src/templates'));
-});
-
-gulp.task('concat', ['templates'], function() {
-  gulp.src(sourceFiles)
-    .pipe(sourcemaps.init())
+gulp.task('concat', function() {
+  return gulp.src('src/*.es6')
+    .pipe(traceur())
     .pipe(concat('abacus.js'))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('dist/'));
+    .pipe(gulp.dest(__dirname + '/dist/'));
 });
-
-gulp.task('minify', ['templates'], function() {
-  gulp.src(sourceFiles)
-    .pipe(sourcemaps.init())
-    .pipe(concat('abacus.min.js'))
-    .pipe(uglify())
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('dist/'));
-});
- 
-gulp.task('jshint', function() {
-  gulp.src(sourceFiles)
-    .pipe(cached('js'))
-    .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish'));
-});
-
-gulp.task('bower', function() {
-  bower();
-});
-
-gulp.task('build', ['jshint', 'concat', 'minify']);
 
 gulp.task('listen', function() {
   connect.server({
@@ -71,8 +21,8 @@ gulp.task('listen', function() {
   });
 });
 
-gulp.task('watch', ['bower', 'build', 'listen'], function() {
-  gulp.watch(sourceFiles.concat(templates), ['build']);
+gulp.task('watch', ['build', 'listen'], function() {
+  gulp.watch(sourceFiles, ['build']);
 });
 
 
